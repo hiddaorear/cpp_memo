56b5776c0d295f7e9dfd486d2da5f9c27a959f38
Siegel Wang <siegelwang@tencent.com>
2019-04-20 00:58:04 +0800

    completed algorithms/full_alignment

--- algorithms/full_alignment.cc
+++ algorithms/full_alignment.cc
@@ -7,7 +7,12 @@ int main()
 {
     int elements[] = {1, 2, 3, 4};
     const size_t N = sizeof(elements)/sizeof(elements[0]);
+    std::cout << "sizeof elements: " << sizeof(elements) << std::endl;
+    std::cout << "sizeof elements[0]: " << sizeof(elements[0]) << std::endl;
     std::vector<int> vec(elements, elements + N);
+    for (auto i: vec) {
+        std::cout << "vec " << i << std::endl;
+    }
     int count = 0;
     do
     {
